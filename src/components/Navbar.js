0a38565412cb879c2b7fe075a1b8530f1a170cd2
ysagar558 <ysagar558@gmail.com>
2025-11-18 16:08:53 +0530

    Adding navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+const AppNavbar = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="/">Simple login page</Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="navbar-nav" />
+
+                <Navbar.Collapse id="navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav.Link href="/about">About</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default AppNavbar;
